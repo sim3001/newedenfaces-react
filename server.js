bc7267724a6a4a71830140f008b50e2d45826428
simranjit.boyal <simranjit.boyal@gmail.com>
2018-01-14 13:48:55 -0500

    Setup Basic Express server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(port,function(){
+  console.log(`Express server listening on port ${port}`);
+});
