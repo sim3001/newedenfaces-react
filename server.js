19b447185054cc8bf970509b629e5ad5add45af0
simranjit.boyal <simranjit.boyal@gmail.com>
2018-01-16 00:01:36 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,107 +1,41 @@
 // Babel ES6/JSX Compiler
 require('babel-register');
-var express = require('express');
+
 var path = require('path');
-var logger = require('morgan');
+var express = require('express');
 var bodyParser = require('body-parser');
-
-var mongoose = require('mongoose');
-var Character = require('./models/character');
-var config = require('./config');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var async = require('async');
+var colors = require('colors');
+var mongoose = require('mongoose');
 var request = require('request');
-var xml2js = require('xml2js');
-var _ = require('underscore');
-
-var swig  = require('swig');
 var React = require('react');
 var ReactDOM = require('react-dom/server');
 var Router = require('react-router');
+var swig  = require('swig');
+var xml2js = require('xml2js');
+var _ = require('underscore');
+
+var config = require('./config');
 var routes = require('./app/routes');
+var Character = require('./models/character');
 
 var app = express();
 
 mongoose.connect(config.database);
 mongoose.connection.on('error', function() {
-  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
 });
 
 app.set('port', process.env.PORT || 3000);
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-/**
- * POST /api/characters
- * Adds new character to the database.
- */
-app.post('/api/characters', function(req, res, next) {
-  var gender = req.body.gender;
-  var characterName = req.body.name;
-  var characterIdLookupUrl = 'https://api.eveonline.com/eve/CharacterID.xml.aspx?names=' + characterName;
-
-  var parser = new xml2js.Parser();
-
-  async.waterfall([
-    function(callback) {
-      request.get(characterIdLookupUrl, function(err, request, xml) {
-        if (err) return next(err);
-        parser.parseString(xml, function(err, parsedXml) {
-          if (err) return next(err);
-          try {
-            var characterId = parsedXml.eveapi.result[0].rowset[0].row[0].$.characterID;
-
-            Character.findOne({ characterId: characterId }, function(err, character) {
-              if (err) return next(err);
-
-              if (character) {
-                return res.status(409).send({ message: character.name + ' is already in the database.' });
-              }
-
-              callback(err, characterId);
-            });
-          } catch (e) {
-            return res.status(400).send({ message: 'XML Parse Error' });
-          }
-        });
-      });
-    },
-    function(characterId) {
-      var characterInfoUrl = 'https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID=' + characterId;
-
-      request.get({ url: characterInfoUrl }, function(err, request, xml) {
-        if (err) return next(err);
-        parser.parseString(xml, function(err, parsedXml) {
-          if (err) return res.send(err);
-          try {
-            var name = parsedXml.eveapi.result[0].characterName[0];
-            var race = parsedXml.eveapi.result[0].race[0];
-            var bloodline = parsedXml.eveapi.result[0].bloodline[0];
-
-            var character = new Character({
-              characterId: characterId,
-              name: name,
-              race: race,
-              bloodline: bloodline,
-              gender: gender,
-              random: [Math.random(), 0]
-            });
-
-            character.save(function(err) {
-              if (err) return next(err);
-              res.send({ message: characterName + ' has been added successfully!' });
-            });
-          } catch (e) {
-            res.status(404).send({ message: characterName + ' is not a registered citizen of New Eden.' });
-          }
-        });
-      });
-    }
-  ]);
-});
 
 /**
  * GET /api/characters
@@ -211,6 +145,50 @@ app.put('/api/characters', function(req, res, next) {
 });
 
 /**
+ * GET /api/characters/shame
+ * Returns 100 lowest ranked characters.
+ */
+app.get('/api/characters/shame', function(req, res, next) {
+  Character
+    .find()
+    .sort('-losses')
+    .limit(100)
+    .exec(function(err, characters) {
+      if (err) return next(err);
+      res.send(characters);
+    });
+});
+
+/**
+ * GET /api/characters/top
+ * Return 100 highest ranked characters. Filter by gender, race and bloodline.
+ */
+app.get('/api/characters/top', function(req, res, next) {
+  var params = req.query;
+  var conditions = {};
+
+  _.each(params, function(value, key) {
+    conditions[key] = new RegExp('^' + value + '$', 'i');
+  });
+
+  Character
+    .find(conditions)
+    .sort('-wins')
+    .limit(100)
+    .exec(function(err, characters) {
+      if (err) return next(err);
+
+      characters.sort(function(a, b) {
+        if (a.wins / (a.wins + a.losses) < b.wins / (b.wins + b.losses)) { return 1; }
+        if (a.wins / (a.wins + a.losses) > b.wins / (b.wins + b.losses)) { return -1; }
+        return 0;
+      });
+
+      res.send(characters);
+    });
+});
+
+/**
  * GET /api/characters/count
  * Returns the total number of characters.
  */
@@ -240,69 +218,6 @@ app.get('/api/characters/search', function(req, res, next) {
 });
 
 /**
- * GET /api/characters/top
- * Return 100 highest ranked characters. Filter by gender, race and bloodline.
- */
-app.get('/api/characters/top', function(req, res, next) {
-  var params = req.query;
-  var conditions = {};
-
-  _.each(params, function(value, key) {
-    conditions[key] = new RegExp('^' + value + '$', 'i');
-  });
-
-  Character
-    .find(conditions)
-    .sort('-wins') // Sort in descending order (highest wins on top)
-    .limit(100)
-    .exec(function(err, characters) {
-      if (err) return next(err);
-
-      // Sort by winning percentage
-      characters.sort(function(a, b) {
-        if (a.wins / (a.wins + a.losses) < b.wins / (b.wins + b.losses)) { return 1; }
-        if (a.wins / (a.wins + a.losses) > b.wins / (b.wins + b.losses)) { return -1; }
-        return 0;
-      });
-
-      res.send(characters);
-    });
-});
-
-
-
-app.use(function(req, res) {
-  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
-    if (err) {
-      res.status(500).send(err.message)
-    } else if (redirectLocation) {
-      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {
-      var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
-      var page = swig.renderFile('views/index.html', { html: html });
-      res.status(200).send(page);
-    } else {
-      res.status(404).send('Page Not Found')
-    }
-  });
-});
-
-/**
- * GET /api/characters/shame
- * Returns 100 lowest ranked characters.
- */
-app.get('/api/characters/shame', function(req, res, next) {
-  Character
-    .find()
-    .sort('-losses')
-    .limit(100)
-    .exec(function(err, characters) {
-      if (err) return next(err);
-      res.send(characters);
-    });
-});
-
-/**
  * GET /api/characters/:id
  * Returns detailed character information.
  */
@@ -321,31 +236,72 @@ app.get('/api/characters/:id', function(req, res, next) {
 });
 
 /**
- * POST /api/report
- * Reports a character. Character is removed after 4 reports.
+ * POST /api/characters
+ * Adds new character to the database.
  */
-app.post('/api/report', function(req, res, next) {
-  var characterId = req.body.characterId;
+app.post('/api/characters', function(req, res, next) {
+  var gender = req.body.gender;
+  var characterName = req.body.name;
+  var characterIdLookupUrl = 'https://api.eveonline.com/eve/CharacterID.xml.aspx?names=' + characterName;
 
-  Character.findOne({ characterId: characterId }, function(err, character) {
-    if (err) return next(err);
+  var parser = new xml2js.Parser();
 
-    if (!character) {
-      return res.status(404).send({ message: 'Character not found.' });
+  async.waterfall([
+    function(callback) {
+      request.get(characterIdLookupUrl, function(err, request, xml) {
+        if (err) return next(err);
+        parser.parseString(xml, function(err, parsedXml) {
+          if (err) return next(err);
+          try {
+            var characterId = parsedXml.eveapi.result[0].rowset[0].row[0].$.characterID;
+
+            Character.findOne({ characterId: characterId }, function(err, character) {
+              if (err) return next(err);
+
+              if (character) {
+                return res.status(409).send({ message: character.name + ' is already in the database.' });
+              }
+
+              callback(err, characterId);
+            });
+          } catch (e) {
+            return res.status(400).send({ message: 'XML Parse Error' });
+          }
+        });
+      });
+    },
+    function(characterId) {
+      var characterInfoUrl = 'https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID=' + characterId;
+
+      request.get({ url: characterInfoUrl }, function(err, request, xml) {
+        if (err) return next(err);
+        parser.parseString(xml, function(err, parsedXml) {
+          if (err) return res.send(err);
+          try {
+            var name = parsedXml.eveapi.result[0].characterName[0];
+            var race = parsedXml.eveapi.result[0].race[0];
+            var bloodline = parsedXml.eveapi.result[0].bloodline[0];
+
+            var character = new Character({
+              characterId: characterId,
+              name: name,
+              race: race,
+              bloodline: bloodline,
+              gender: gender,
+              random: [Math.random(), 0]
+            });
+
+            character.save(function(err) {
+              if (err) return next(err);
+              res.send({ message: characterName + ' has been added successfully!' });
+            });
+          } catch (e) {
+            res.status(404).send({ message: characterName + ' is not a registered citizen of New Eden.' });
+          }
+        });
+      });
     }
-
-    character.reports++;
-
-    if (character.reports > 4) {
-      character.remove();
-      return res.send({ message: character.name + ' has been deleted.' });
-    }
-
-    character.save(function(err) {
-      if (err) return next(err);
-      res.send({ message: character.name + ' has been reported.' });
-    });
-  });
+  ]);
 });
 
 /**
@@ -452,6 +408,55 @@ app.get('/api/stats', function(req, res, next) {
 });
 
 
+/**
+ * POST /api/report
+ * Reports a character. Character is removed after 4 reports.
+ */
+app.post('/api/report', function(req, res, next) {
+  var characterId = req.body.characterId;
+
+  Character.findOne({ characterId: characterId }, function(err, character) {
+    if (err) return next(err);
+
+    if (!character) {
+      return res.status(404).send({ message: 'Character not found.' });
+    }
+
+    character.reports++;
+
+    if (character.reports > 4) {
+      character.remove();
+      return res.send({ message: character.name + ' has been deleted.' });
+    }
+
+    character.save(function(err) {
+      if (err) return next(err);
+      res.send({ message: character.name + ' has been reported.' });
+    });
+  });
+});
+
+app.use(function(req, res) {
+  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirectLocation) {
+      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+        var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+        var page = swig.renderFile('views/index.html', { html: html });
+        res.status(200).send(page);
+    } else {
+      res.status(404).send('Page Not Found')
+    }
+  });
+});
+
+app.use(function(err, req, res, next) {
+  console.log(err.stack.red);
+  res.status(err.status || 500);
+  res.send({ message: err.message });
+});
 
 /**
  * Socket.io stuff.
